6c2c5905f4c594c0fd74aad6d26d68d335fcebd9
PJH <carpediem.mementomori.1984@gmail.com>
2021-03-03 13:54:31 +0900

    Merge: styled component초기세팅 merged

---
+++ src/FormLayout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Signup from '../src/Pages/Signup/Signup';
+import Login from '../src/Pages/Login/Login';
+import './FormLayout.scss';
+
+export default function FormLayout() {
+  return <div></div>;
+}
