01248c908e32f187dd670d1b93e7eafa88666097
PJH <carpediem.mementomori.1984@gmail.com>
2021-03-03 13:54:31 +0900

    Merge: styled component초기세팅 merged
    Add: styled-components 설치 및 파일 셋업
    
    Add: styled-components 초기 세팅 완료
    
    Add: (리베이스) 레이아웃 완료 및 style component & hooks 적용완료
    
    Add: styled component 적용 중
    
    Add: 회원가입 및 로그인 UI 제작 완료
    
    Modify: styled component 적용중
    
    Modify: scss를 styled component로 대체
    
    Modify: styled component 및 hooks 적용완료

---
+++ src/signInData.js
@@ -0,0 +1,18 @@
+const signInProps = {
+  title: '로그인',
+  input: [
+    {
+      text: '이메일',
+      type: 'email',
+      placeholder: 'example@example.com',
+    },
+    {
+      text: '비밀번호',
+      type: 'password',
+      placeholder: '*******',
+    },
+  ],
+  button: ['로그인', '카카오로 3초만에 시작하기'],
+};
+
+export default signInProps;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  *, html, body{
+    box-sizing:border-box;
+    font-family: "Noto Sans KR", sans-serif;
+    &:focus {
+      outline: none;
+    }
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    border: none;
+  }
+`;
+
+export default GlobalStyle;
