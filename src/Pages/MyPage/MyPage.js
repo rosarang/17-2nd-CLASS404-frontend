e8a70d4e64a7e9d6d0e42aba5efdc76e60c3f26a
Sarang Lee <rosaranglee@gmail.com>
2021-03-07 12:12:09 +0900

    Add: MyPage 컴포넌트 생성

---
+++ src/Pages/MyPage/MyPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function MyPage(props) {
+  return <MyPageWrapper></MyPageWrapper>;
+}
+
+const MyPageWrapper = styled.div``;
+
+export default MyPage;
