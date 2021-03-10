eebb35ab67735c9a90df78447ad499a018fc22f0
PJH <carpediem.mementomori.1984@gmail.com>
2021-03-10 12:25:34 +0900

    Add: 레이아웃 구현 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Main from "./Pages/Main/Main";
-import Login from "./Pages/Login/Login";
-import Signup from "./Pages/Signup/Signup";
-import CreatorApply from "./Pages/CreatorApply/CreatorApply";
-import ProductDetail from "./Pages/ProductDetail/ProductDetail";
-// import Search from "./Pages/Search/Search";
+import Main from './Pages/Main/Main';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Signup/Signup';
+import CreatorApply from './Pages/CreatorApply/CreatorApply';
+import ProductDetail from './Pages/ProductDetail/ProductDetail';
+import VideoPlay from './Pages/VideoPlay/VideoPlay';
 
 class Routes extends Component {
   render() {
@@ -19,7 +19,7 @@ class Routes extends Component {
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/apply" component={CreatorApply} />
           <Route exact path="/product" component={ProductDetail} />
-          {/* <Route exact path="/search" component={Search} /> */}
+          <Route exact path="/video" component={VideoPlay} />
         </Switch>
       </Router>
     );
