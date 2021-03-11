9c62738387ae66852086cb59ca921dabcdd5fe6f
jieunlee <jenna8447@naver.com>
2021-03-11 14:53:23 +0900

    Add: creatorApply 레이아웃 완료
    - ProductList, Product, CategoryModal, SortingModal, Filter, Checkbox, RadioBtn, Tag 컴포넌트 추가
    - product, category, sorting  목데이터 추가
    - config API 및 목데이터 주소 추가
    - favicon 추가
    - title명 수정
    - category 데이터 수정
    - config api 주소 수정
    - 필터 및 정렬 함수 수정
    - 멘토 리뷰 반영
    - index.html conflict 해결

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import ReviewUpload from "./Pages/ReveiwUpload/ReviewUpload";
+import ProductDetail from "./Pages/ProductDetail/ProductDetail";
 import Main from "./Pages/Main/Main";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import CreatorApply from "./Pages/CreatorApply/CreatorApply";
-import ProductDetail from "./Pages/ProductDetail/ProductDetail";
-import ReviewUpload from "./Pages/ReveiwUpload/ReviewUpload";
 import MyPage from "./Pages/MyPage/MyPage";
 
 class Routes extends Component {
@@ -15,14 +14,12 @@ class Routes extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
-          {/* <Route exact path="/:id" component={Main} /> */}
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/apply" component={CreatorApply} />
           <Route exact path="/product" component={ProductDetail} />
           <Route exact path="/mypage" component={MyPage} />
-          {/* <Route exact path="/product" component={ProductDetail} /> */}
-          {/* <Route exact path="/product:id" component={ProductDetail} /> */}
+          <Route exact path="/product:id" component={ProductDetail} />
           <Route exact path="/reviewupload" component={ReviewUpload} />
         </Switch>
       </Router>
